29e5390d41710d223c47c5bc7048933a23fc2888
Artem Pustelnikov <pustelnikov@gmail.com>
2025-05-17 13:43:45 +0300

    Added Authentication

---
+++ room-service/src/main/java/aston/room_booking/room_service/utilities/AuthenticationFilter.java
@@ -0,0 +1,25 @@
+package aston.room_booking.room_service.utilities;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.core.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@Component
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
